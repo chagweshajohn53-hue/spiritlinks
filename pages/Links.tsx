15b6c461667fdfdf4ba1a02f0fcc306a52129fa3
unknown <Chagweshajohn53@gmail.com>
2026-02-14 13:45:18 +0200

    Initial commit

--- pages/Links.tsx
+++ pages/Links.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { mockDb } from '../services/mockDb';
+import { dataService } from '../services/dataService';
 import { LinkItem } from '../types';
 import { ExternalLink, Search } from 'lucide-react';
 
